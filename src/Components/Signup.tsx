a5fbb78b8f17848e397794ab559efd576314fdea
sangeeta8642 <sangeetaperagani8642@gmail.com>
2024-05-16 20:51:53 +0530

    signup

--- src/Components/Signup.tsx
+++ src/Components/Signup.tsx
@@ -54,7 +54,7 @@ export default function Signup() {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/signup", {
+      const response = await fetch("https://tradingbackend-2w6s.onrender.com/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
